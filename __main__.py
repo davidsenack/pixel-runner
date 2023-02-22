84b467fba381b5fc3a96d9cf93f363047f5afc2f
David Senack <david.senack@gmail.com>
2023-02-22 15:12:04 -0500

    Adjust styling of score text

--- __main__.py
+++ __main__.py
@@ -11,7 +11,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surface = test_font.render('Score: 0', False, 'Black').convert()
+score_surface = test_font.render('Score: 0', False, (64, 64, 64)).convert()
 score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
@@ -37,7 +37,7 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
 
-    pygame.draw.rect(screen, 'Pink', score_rect)
+    pygame.draw.rect(screen, '#c0e8ec', score_rect)
     screen.blit(score_surface, score_rect)
 
     snail_rect = snail_rect.move(-4, 0)
