347ddbea41334f16212e9bea468730007aade9ae
David Senack <david.senack@gmail.com>
2023-02-22 11:47:06 -0500

    Format code to meet pep8

--- __main__.py
+++ __main__.py
@@ -1,19 +1,20 @@
 import __main__
 import pygame
 from sys import exit
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 
 while True:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-      exit()
-    if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_ESCAPE:
-        pygame.quit()
-        exit()
-  #draw all our elements
-  #update everything
-  pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
