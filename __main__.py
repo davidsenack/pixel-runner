53c6503b17cb02048c21ddc007364a92f2254cf3
David Senack <david.senack@gmail.com>
2023-02-22 13:51:16 -0500

    Add characters as rectangle objects

--- __main__.py
+++ __main__.py
@@ -13,7 +13,11 @@ ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My Game', False, 'Black').convert()
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_rect = snail_surface.get_rect(midbottom=(600, 300))
+#snail_x_pos = 600
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80, 300))
 
 
 while True:
@@ -30,9 +34,12 @@ while True:
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
     
-    snail_x_pos -= 4
-    if snail_x_pos < -100: snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 250))
+    if snail_rect.x <= -100:
+        snail_rect.x = 800
+    snail_rect = snail_rect.move(-4, 0)
+    screen.blit(snail_surface, snail_rect)
+    
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(60)
