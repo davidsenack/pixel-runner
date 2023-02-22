9e913fb05d799721c755191dd94086c08e9f9781
David Senack <david.senack@gmail.com>
2023-02-22 12:24:06 -0500

    Add test surface to display

--- __main__.py
+++ __main__.py
@@ -7,6 +7,9 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,7 +19,8 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 exit()
-    # draw all our elements
-    # update everything
+    
+    screen.blit(test_surface, (200, 100))
+
     pygame.display.update()
     clock.tick(60)
