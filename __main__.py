5bb8f16ded66fdeeb3f80c7b0a71293eb5181e27
David Senack <david.senack@gmail.com>
2023-02-22 18:28:37 -0500

    Add scoring system functionality

--- __main__.py
+++ __main__.py
@@ -2,18 +2,26 @@ import __main__
 import pygame
 from sys import exit
 
+def display_score():
+    current_time = int(pygame.time.get_ticks() / 1000) - start_time
+    score_surface = test_font.render(f'Score: {current_time}', False, (64, 64, 64)).convert()
+    score_rect = score_surface.get_rect(center=(400, 50))
+    screen.blit(score_surface, score_rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 game_active = True
+start_time = 0
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surface = test_font.render('Score: 0', False, (64, 64, 64)).convert()
-score_rect = score_surface.get_rect(center=(400, 50))
+# score_surface = test_font.render('Score: 0', False, (64, 64, 64)).convert()
+# score_rect = score_surface.get_rect(center=(400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(600, 300))
@@ -50,21 +58,24 @@ while True:
                     game_active = True
                     player_rect.midbottom = (80, 300)
                     snail_rect.midbottom = (600, 300)
+                    start_time = int(pygame.time.get_ticks() / 1000)
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     game_active = True
                     player_rect.midbottom = (80, 300)
                     snail_rect.midbottom = (600, 300)
-        
+                    start_time = int(pygame.time.get_ticks() / 1000)
+
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
 
         #pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        screen.blit(score_surface, score_rect)
+        #screen.blit(score_surface, score_rect)
+        display_score()
 
-        snail_rect = snail_rect.move(-4, 0)
+        snail_rect = snail_rect.move(-6, 0)
         if snail_rect.x <= -100: snail_rect.x = 800
         screen.blit(snail_surface, snail_rect)
         
