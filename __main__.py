03638ec8fa0fbcea4ef9c5bd2b16d2eb880af795
David Senack <david.senack@gmail.com>
2023-02-22 11:42:17 -0500

    Change game title

--- __main__.py
+++ __main__.py
@@ -1,18 +1,19 @@
 import __main__
 import pygame
-
+from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runner")
 
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
-      quit()
+      exit()
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_ESCAPE:
         pygame.quit()
-        quit()
+        exit()
   #draw all our elements
   #update everything
   pygame.display.update()
